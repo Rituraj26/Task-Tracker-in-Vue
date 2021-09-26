51dd7f97f6a4c15aab761371b838646369498e2c
Rituraj <riturajd265@gmail.com>
2021-09-26 23:06:04 +0530

    vue draggable setup

--- src/components/Tasks.vue
+++ src/components/Tasks.vue
@@ -1,18 +1,13 @@
 <template>
-    <!-- <draggable
-        :list="tasks"
-        :move="checkMove"
-        @start="dragging = true"
-        @end="dragging = false"
-    > -->
-    <div :key="task.id" v-for="task in tasks">
-        <Task
-            @dblclick="$emit('toggle-reminder', task.id)"
-            @delete-task="$emit('delete-task', task.id)"
-            :task="task"
-        />
-    </div>
-    <!-- </draggable> -->
+    <draggable v-if="tasks" :list="tasks">
+        <div :key="task.id" v-for="task in tasks">
+            <Task
+                @dblclick="$emit('toggle-reminder', task.id)"
+                @delete-task="$emit('delete-task', task.id)"
+                :task="task"
+            />
+        </div>
+    </draggable>
 </template>
 
 <script>
